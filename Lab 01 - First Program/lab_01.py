5883cc61e3490e7ec4919bd76832e04556b70c7c
KylieFurner <kfurner002@gmail.com>
2022-10-28 11:50:17 -0600

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,3 @@
-print("Hello World")
+print("Hello World, This is my first program using PyCharm and Arcade")
+print("I\'ve learned how to use the print function and how to push to Github")
+print("You need the commands: \n1: git add -A \n2: git commit -m \"Work on lab 1\" \n3: git push")
